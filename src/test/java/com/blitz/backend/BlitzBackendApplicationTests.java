86ef86f7e5f37a10146a1d3fef92fb10514be7d5
Harshith <harshithl1777@gmail.com>
2023-11-04 16:52:14 -0400

    feat: scaffold boilerplate spring boot API

---
+++ src/test/java/com/blitz/backend/BlitzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blitz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlitzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
