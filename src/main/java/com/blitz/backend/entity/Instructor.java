d353a1086388c0ba1af8658d5cc6c4754e478c79
vivic04 <vihaanchugh2811@gmail.com>
2023-11-09 13:16:30 -0500

    Added interface implementation

---
+++ src/main/java/com/blitz/backend/entity/Instructor.java
@@ -0,0 +1,8 @@
+package com.blitz.backend.entity;
+
+public class Instructor implements User{
+    String firstName;
+    String lastName;
+    String emailAddress;
+
+}
--- src/main/java/com/blitz/backend/entity/Student.java
+++ src/main/java/com/blitz/backend/entity/Student.java
@@ -1,15 +1,20 @@
 package com.blitz.backend.entity;
 
-public interface User {
-
+public class Student implements User {
+    String firstName;
+    String lastName;
+    String emailAddress;
+    String studentNumber;
 
     public static void getFullName(){
     }
 
+    public static void isValidStudent(){
+
+    }
     public static void sendEmailConfirmation(){
     }
 
     public static void updateEmailAddress(){
     }
-
 }
