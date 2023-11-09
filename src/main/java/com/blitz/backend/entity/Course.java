7f123a0f99a72d5303e529b740f0d8e722a1d41f
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-11-09 16:40:58 -0500

    added course entity and its instance attributes and methods

---
+++ src/main/java/com/blitz/backend/entity/Course.java
@@ -0,0 +1,19 @@
+package com.blitz.backend.entity;
+
+import java.util.List;
+
+public class Course {
+    String courseCode;
+    String instructor;
+    List<String> days;
+    List<String> times;
+
+    public static void getCourseCode(){
+    }
+    public static void markAttendance(){
+    }
+    public static void getSchedule(){
+    }
+    public static void getAttendanceReport(){
+    }
+}
