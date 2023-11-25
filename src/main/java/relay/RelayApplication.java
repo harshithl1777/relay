f6ac67c5382b398072ac4a30103dda7e9eee46f8
Harshith <harshithl1777@gmail.com>
2023-11-24 22:07:25 -0500

    feat: install firebase dependency, add config code

---
+++ src/main/java/relay/RelayApplication.java
@@ -0,0 +1,44 @@
+package relay;
+
+import java.io.FileInputStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+@RestController
+@SpringBootApplication
+public class RelayApplication {
+
+	private static String FIREBASE_DATABASE_URL = "https:blitz-web1.firebaseio.com/";
+	private static String FIREBASE_CREDENTIALS_FILE = "src/main/resources/firebase.json";
+
+	@RequestMapping("/health")
+	String health() {
+		return "200 OK";
+	}
+
+	private static void configureFirebase() {
+		try {
+			FileInputStream FirebaseCredentials = new FileInputStream(FIREBASE_CREDENTIALS_FILE);
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(FirebaseCredentials))
+					.setDatabaseUrl(FIREBASE_DATABASE_URL)
+					.build();
+
+			FirebaseApp.initializeApp(options);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		configureFirebase();
+		SpringApplication.run(RelayApplication.class, args);
+	}
+}
