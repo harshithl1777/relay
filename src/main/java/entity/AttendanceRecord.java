553f2ca525808799e21567120ffe10d716e48c6f
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-11-25 13:38:45 -0500

    updated the class based on firebase database

--- src/main/java/entity/AttendanceRecord.java
+++ src/main/java/entity/AttendanceRecord.java
@@ -1,18 +1,27 @@
 package entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class AttendanceRecord {
-    String courseCode;
-    String date;
-    String time;
-    Instructor instructor;
-    String section;
-    Session attendanceSession;
-    public AttendanceRecord(String courseCode, String date, String time, Instructor instructor, String section, Session attendanceSession) {
+    private String courseCode;
+    private LocalDate date;
+    private LocalDateTime time;
+    private Instructor instructor;
+    private String section;
+    private Session attendanceSession;
+    private List<String> studentsPresent;
+
+    public AttendanceRecord(String courseCode, LocalDate date, LocalDateTime time, Instructor instructor, String section, Session attendanceSession, List<String> studentsPresent) {
+
         this.courseCode = courseCode;
         this.date = date;
         this.time = time;
         this.instructor = instructor;
         this.section = section;
         this.attendanceSession = attendanceSession;
+        this.studentsPresent = studentsPresent;
     }
+
 }
