8d8a2d63c0721455f38d7c72b9e4effcf90348ed
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-11-25 21:33:55 -0500

    Updated classes based on requested changes

--- src/main/java/relay/entities/AttendanceRecord.java
+++ src/main/java/relay/entities/AttendanceRecord.java
@@ -1,28 +1,39 @@
-package entity;
+package relay.entities;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class AttendanceRecord {
     private String courseCode;
-    private LocalDate date;
-    private LocalDateTime time;
-    private Instructor instructor;
-    private String section;
-    private Session attendanceSession;
     private ArrayList<String> studentsPresent;
+    private Student student;
 
-    // Constructor
-    public AttendanceRecord(String courseCode, LocalDate date, LocalDateTime time, Instructor instructor, String section, Session attendanceSession, ArrayList<String> studentsPresent) {
-
+    public AttendanceRecord(String courseCode, ArrayList<String> studentsPresent, Student student) {
         this.courseCode = courseCode;
-        this.date = date;
-        this.time = time;
-        this.instructor = instructor;
-        this.section = section;
-        this.attendanceSession = attendanceSession;
+        this.studentsPresent = studentsPresent;
+        this.student = student;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public ArrayList<String> getStudentsPresent() {
+        return studentsPresent;
+    }
+
+    public void setStudentsPresent(ArrayList<String> studentsPresent) {
         this.studentsPresent = studentsPresent;
     }
 
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
