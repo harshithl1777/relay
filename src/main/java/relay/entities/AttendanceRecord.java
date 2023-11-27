4dce94d54b7b2700c700dbd12abd745d3ee3bf2e
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-11-27 18:25:53 -0500

    updated based on requested changes

--- src/main/java/relay/entities/AttendanceRecord.java
+++ src/main/java/relay/entities/AttendanceRecord.java
@@ -3,30 +3,10 @@ package relay.entities;
 import java.util.ArrayList;
 
 public class AttendanceRecord {
-    private String courseCode;
-    private ArrayList<String> studentsPresent;
     private Student student;
 
     public AttendanceRecord(String courseCode, ArrayList<String> studentsPresent, Student student) {
-        this.courseCode = courseCode;
-        this.studentsPresent = studentsPresent;
         this.student = student;
-    }
-
-    public String getCourseCode() {
-        return courseCode;
-    }
-
-    public void setCourseCode(String courseCode) {
-        this.courseCode = courseCode;
-    }
-
-    public ArrayList<String> getStudentsPresent() {
-        return studentsPresent;
-    }
-
-    public void setStudentsPresent(ArrayList<String> studentsPresent) {
-        this.studentsPresent = studentsPresent;
     }
 
     public Student getStudent() {
