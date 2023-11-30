e9f32a134bca52fe1a659732e222fc2d1f70aa44
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-11-29 23:56:45 -0500

    Included java docs for FirestoreDatabaseTest

--- src/test/java/relay/data_access/FirestoreDatabaseTest.java
+++ src/test/java/relay/data_access/FirestoreDatabaseTest.java
@@ -7,12 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FirestoreDatabaseTest {
 
+    /**
+     * Test to verify that the {@link FirestoreDatabase#get()} method returns a
+     * non-null Firestore reference.
+     */
     @Test
     public void testGetReturnsNonNullFirestoreReference() {
         Firestore firestore = FirestoreDatabase.get();
         assertNotNull(firestore);
     }
 
+    /**
+     * Test to check that consecutive calls to {@link FirestoreDatabase#get()}
+     * return the same Firestore reference, ensuring it is a singleton.
+     */
     @Test
     public void testReturnsSingularFirestoreReference() {
         Firestore firestore1 = FirestoreDatabase.get();
@@ -21,7 +29,6 @@ public class FirestoreDatabaseTest {
         assertNotNull(firestore1);
         assertNotNull(firestore2);
         assertSame(firestore1, firestore2);
-
 
     }
 
