02178fda3362dbc5b08a6600588be253190c1303
Muhammad Ibrahim <ibrahim.baluch@gmail.com>
2023-11-30 18:37:15 -0500

    Refactored DataAccessObject to CourseDataAccessObject

--- src/main/java/relay/RelayApplication.java
+++ src/main/java/relay/RelayApplication.java
@@ -1,7 +1,6 @@
 package relay;
 
 import java.io.FileInputStream;
-import java.util.ArrayList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import relay.data_access.FirebaseDataAccessObject;
+import relay.data_access.FirebaseCourseDataAccessObject;
 import relay.entity.Course;
 import relay.entity.Instructor;
 
@@ -41,7 +40,7 @@ public class RelayApplication {
 
 	public static void main(String[] args) {
 		configureFirebase();
-		FirebaseDataAccessObject dao = new FirebaseDataAccessObject();
+		FirebaseCourseDataAccessObject dao = new FirebaseCourseDataAccessObject();
 		Instructor instructor = new Instructor("Joe", "Smith", "joe@hotmail.com");
 		Course course = new Course("2", "MAT137", instructor);
 
--- src/main/java/relay/data_access/FirebaseCourseDataAccessObject.java
+++ src/main/java/relay/data_access/FirebaseCourseDataAccessObject.java
@@ -2,31 +2,25 @@
 package relay.data_access;
 
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 
 import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import relay.entity.Course;
-import relay.entity.Instructor;
-
-import java.io.FileInputStream;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 
-public class FirebaseDataAccessObject {
+public class FirebaseCourseDataAccessObject {
     private static String FIREBASE_DATABASE_URL = "https:blitz-web1.firebaseio.com/";
     private static String FIREBASE_CREDENTIALS_FILE = "src/main/resources/firebase.json";
 
 
     private final Firestore db;
 
-    public FirebaseDataAccessObject() {
+    public FirebaseCourseDataAccessObject() {
 //        configureFirebase();
         db = FirestoreClient.getFirestore();
 
