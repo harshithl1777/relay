82a64cd77fca487f374c0bef40a0f73896e7c4f5
Harshith Latchupatula <harshithl1777@gmail.com>
2023-12-01 02:50:55 -0500

    fix: change output and input paths in decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_DECRYPTION_KEY" \
---output $HOME/src/main/resources/test.json $HOME/src/main/resources/firebase.json.gpg
+--output ./src/main/resources/test.json ./src/main/resources/firebase.json.gpg
