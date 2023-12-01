c5e5c9735916ed38e929f42da9e4b79c7bca5608
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-12-01 01:27:00 -0500

    Implemented exists method in firebase instructor DAO

--- src/main/java/relay/data_access/FirebaseInstructorDataAccessObject.java
+++ src/main/java/relay/data_access/FirebaseInstructorDataAccessObject.java
@@ -2,29 +2,28 @@ package relay.data_access;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
-
 import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.WriteResult;
 import relay.entity.Instructor;
-import relay.entity.InstructorFactory;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-
-
+/**
+ * Provides data access methods for interacting with the Firebase Firestore database
+ * to perform CRUD operations on Instructor entities.
+ */
 public class FirebaseInstructorDataAccessObject {
-    private final InstructorFactory instructorFactory;
 
-    public FirebaseInstructorDataAccessObject(InstructorFactory instructorFactory) {
-        this.instructorFactory = instructorFactory;
-    }
-
-
-    public boolean create(Instructor instructor) {
-        ApiFuture<DocumentReference> addedDocRef = FirestoreSingleton.get().collection("instructors").add(instructor);
+    /**
+     * Saves the provided Instructor object to the Firestore database.
+     *
+     * @param instructor The Instructor object to be saved.
+     * @return True if the save operation is successful, false otherwise.
+     */
+    public boolean save(Instructor instructor) {
+        ApiFuture<DocumentReference> docRef = FirestoreSingleton.get().collection("instructors").add(instructor);
         try {
-            instructor.setInstructorID(addedDocRef.get().getId());
+            instructor.setInstructorID(docRef.get().getId());
             return true;
         } catch (InterruptedException | ExecutionException e) {
             System.out.println(e);
@@ -32,13 +31,22 @@ public class FirebaseInstructorDataAccessObject {
         }
     }
 
+    /**
+     * Reads an Instructor object from the Firestore database based on the provided instructorID.
+     *
+     * @param instructorID The unique identifier of the Instructor.
+     * @return The retrieved Instructor object, or null if not found or an error occurs.
+     */
     public Instructor read(String instructorID) {
         ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID).get();
 
         try {
             DocumentSnapshot instructorDocument = future.get();
             if (instructorDocument.exists()) {
-                return instructorDocument.toObject(Instructor.class);
+                Instructor instructor = instructorDocument.toObject(Instructor.class);
+                assert instructor != null;
+                instructor.setInstructorID(instructorID);
+                return instructor;
             } else {
                 return null;
             }
@@ -47,18 +55,25 @@ public class FirebaseInstructorDataAccessObject {
             e.printStackTrace();
             return null;
         }
-
-
     }
 
 
-    private Map<String, Object> prepareDocument(Instructor instructor) {
-        Map<String, Object> instructorDocument = new HashMap<>();
-        instructorDocument.put("firstName", instructor.getFirstName());
-        instructorDocument.put("LastName", instructor.getLastName());
-        instructorDocument.put("emailAddress", instructor.getEmailAddress());
-        return instructorDocument;
+    /**
+     * Checks if an Instructor object with the provided instructorID exists in the Firestore database.
+     *
+     * @param instructorID The unique identifier of the Instructor.
+     * @return True if the Instructor exists, false otherwise.
+     */
+    public boolean exists(String instructorID) {
+        ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID).get();
+
+        try {
+            DocumentSnapshot instructorDocument = future.get();
+            return instructorDocument.exists();
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-
-
 }
