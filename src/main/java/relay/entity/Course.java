13506ed1a522825fda581130290f0d5f21c7887a
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-12-01 10:12:34 -0500

    Changed types to List instead of ArrayList

--- src/main/java/relay/entity/Course.java
+++ src/main/java/relay/entity/Course.java
@@ -1,43 +1,44 @@
 package relay.entity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Course {
-	private String courseID;
-	private Instructor instructor;
-	private ArrayList<AttendanceRecord> history;
+    private String courseID;
+    private Instructor instructor;
+    private List<AttendanceRecord> history;
 
-	public Course(String courseID, Instructor instructor) {
-		this.courseID = courseID;
-		this.instructor = instructor;
-		this.history = new ArrayList<>();
-	}
+    public Course(String courseID, Instructor instructor) {
+        this.courseID = courseID;
+        this.instructor = instructor;
+        this.history = new ArrayList<>();
+    }
 
-	public String getCourseID() {
-		return courseID;
-	}
+    public String getCourseID() {
+        return courseID;
+    }
 
-	public Instructor getInstructor() {
-		return instructor;
-	}
+    public Instructor getInstructor() {
+        return instructor;
+    }
 
-	public ArrayList<AttendanceRecord> getHistory() {
-		return history;
-	}
+    public List<AttendanceRecord> getHistory() {
+        return history;
+    }
 
-	public void setCourseID(String courseID) {
-		this.courseID = courseID;
-	}
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
 
-	public void setInstructor(Instructor instructor) {
-		this.instructor = instructor;
-	}
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
 
-	public void setHistory(ArrayList<AttendanceRecord> history) {
-		this.history = history;
-	}
+    public void setHistory(List<AttendanceRecord> history) {
+        this.history = history;
+    }
 
-	public void appendHistory(ArrayList<AttendanceRecord> newRecords) {
-		this.history.addAll(newRecords);
-	}
+    public void appendHistory(List<AttendanceRecord> newRecords) {
+        this.history.addAll(newRecords);
+    }
 }
