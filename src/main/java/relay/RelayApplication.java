84360b11e9074ab1b3e041d6a11d45c3a01d778b
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-12-01 04:07:53 -0500

    resolved conflict by reverting

--- src/main/java/relay/RelayApplication.java
+++ src/main/java/relay/RelayApplication.java
@@ -1,14 +1,15 @@
 package relay;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 @RestController
 @SpringBootApplication
@@ -26,7 +27,6 @@ public class RelayApplication {
 			FileInputStream FirebaseCredentials = new FileInputStream(FIREBASE_CREDENTIALS_FILE);
 			FirebaseOptions options = FirebaseOptions.builder()
 					.setCredentials(GoogleCredentials.fromStream(FirebaseCredentials))
-					.setStorageBucket("blitz-web1.appspot.com")
 					.build();
 
 			FirebaseApp.initializeApp(options);
