dba2a045987bf68789abba92edc5e43432a58d6f
Muhammad Ibrahim <ibrahim.baluch@gmail.com>
2023-12-01 15:59:51 -0500

    converted manual courseID to firebase autogenerated ID as per requested changes

--- src/main/java/relay/RelayApplication.java
+++ src/main/java/relay/RelayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import relay.data_access.FirebaseInitializationSingleton;
 
+
 @RestController
 @SpringBootApplication
 public class RelayApplication {
@@ -17,6 +18,7 @@ public class RelayApplication {
 
 	public static void main(String[] args) {
 		FirebaseInitializationSingleton.initialize();
+
 		SpringApplication.run(RelayApplication.class, args);
 	}
 }
--- src/main/java/relay/data_access/FirebaseCourseDataAccessObject.java
+++ src/main/java/relay/data_access/FirebaseCourseDataAccessObject.java
@@ -14,24 +14,31 @@ import java.util.concurrent.ExecutionException;
 
 
 public class FirebaseCourseDataAccessObject {
+    private final Firestore db;
 
+    public FirebaseCourseDataAccessObject() {
+        this.db = FirestoreSingleton.get();
+    }
 
     /**
-     * Creates a new course document in Firestore based on the given Course object.
+     * Creates a new course document in Firestore and sets the courseID within the provided Course object.
      *
      * @param course The Course object containing course details to be added to Firestore.
+     * @throws RuntimeException If an InterruptedException or ExecutionException occurs during Firestore operations.
      */
     public void createCourse(Course course) {
-        Map<String, Object> courseDocument = new HashMap<>();
-        courseDocument.put("courseID", course.getCourseID());
-        courseDocument.put("courseName", course.getCourseName());
-        courseDocument.put("instructorID", course.getInstructor().getInstructorID());
-
-        ApiFuture<WriteResult> future = FirestoreSingleton.get().collection("courses").document(course.getCourseID()).set(courseDocument);
         try {
-            System.out.println("Update time : " + future.get().getUpdateTime());
-        } catch (InterruptedException | ExecutionException e) {
-            System.out.println(e);
+            Map<String, Object> courseDocument = new HashMap<>();
+            courseDocument.put("courseName", course.getCourseName());
+            courseDocument.put("instructorID", course.getInstructor().getInstructorID());
+
+            ApiFuture<DocumentReference> newCourseDocument = db.collection("courses").add(courseDocument);
+            String courseID = newCourseDocument.get().getId();
+            course.setCourseID(courseID);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
         }
 
     }
@@ -41,11 +48,9 @@ public class FirebaseCourseDataAccessObject {
      *
      * @param instructor The Instructor object for which courses are to be retrieved.
      * @return An ArrayList of Course objects associated with the specified instructor.
-     * @throws ExecutionException   If the execution of the query fails.
-     * @throws InterruptedException If the query execution is interrupted.
+     * @throws RuntimeException If an InterruptedException or ExecutionException occurs during Firestore operations.
      */
     public ArrayList<Course> getCoursesByInstructor(Instructor instructor) {
-        Firestore db = FirestoreSingleton.get();
         Query query = db.collection("courses")
                 .whereEqualTo("instructorID", instructor.getInstructorID());
 
@@ -58,7 +63,7 @@ public class FirebaseCourseDataAccessObject {
 
                 Map<String, Object> courseData = document.getData();
 
-                String courseID = (String) courseData.get("courseID");
+                String courseID = document.getId();
                 String courseName = (String) courseData.get("courseName");
 
                 Course course = new Course(courseID, courseName, instructor);
@@ -79,9 +84,10 @@ public class FirebaseCourseDataAccessObject {
      *
      * @param courseID The ID of the course to check for existence.
      * @return true if the course exists, false otherwise.
+     * @throws RuntimeException If an ExecutionException or InterruptedException occurs during Firestore operations.
      */
     public boolean exists(String courseID) {
-        ApiFuture<DocumentSnapshot> retrievedcourseDocument = FirestoreSingleton.get().collection("courses").document(courseID).get();
+        ApiFuture<DocumentSnapshot> retrievedcourseDocument = db.collection("courses").document(courseID).get();
 
         try {
             DocumentSnapshot courseDocument = retrievedcourseDocument.get();
