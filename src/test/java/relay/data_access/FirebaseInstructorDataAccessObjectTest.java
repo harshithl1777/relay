028f72a33c06417d0da2f20ca4da39b2e74e2b0f
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-11-30 20:51:57 -0500

    Implemented create method

---
+++ src/test/java/relay/data_access/FirebaseInstructorDataAccessObjectTest.java
@@ -0,0 +1,10 @@
+package relay.data_access;
+
+import com.google.cloud.firestore.Firestore;
+import org.junit.Before;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirebaseInstructorDataAccessObjectTest {
+
+}
