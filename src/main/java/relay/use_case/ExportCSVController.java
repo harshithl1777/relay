c24c74ca7f178caddbe5581781607782312a5690
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-02 15:33:01 -0500

    controller implementation

--- src/main/java/relay/use_case/ExportCSVController.java
+++ src/main/java/relay/use_case/ExportCSVController.java
@@ -1,4 +1,15 @@
 package relay.use_case;
 
 public class ExportCSVController {
+
+    final ExportCSVInputBoundary exportCSVInteractor;
+
+    public ExportCSVController(ExportCSVInputBoundary exportCSVInteractor) {
+        this.exportCSVInteractor = exportCSVInteractor;
+    }
+
+    public void execute(ExportCSVInputData inputData) {
+        exportCSVInteractor.execute(inputData);
+
+    }
 }
