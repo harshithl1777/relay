4ac698a8746a4d1923cd874351d21580a415bc2d
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-02 16:41:59 -0500

    Output Data check

--- src/main/java/relay/show_courses/ShowCourseOutputData.java
+++ src/main/java/relay/show_courses/ShowCourseOutputData.java
@@ -1,4 +1,13 @@
 package relay.show_courses;
 
+import relay.entity.AttendanceRecord;
+
+import java.util.List;
+
 public class ShowCourseOutputData {
+    private final List<String> courseNames;
+
+    public ShowCourseOutputData(List<String> courseNames) {
+        this.courseNames = courseNames;
+    }
 }
