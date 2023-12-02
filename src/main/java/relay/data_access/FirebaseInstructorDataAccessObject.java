e28035c77ce39171ceb22df3b4017b826c150627
Harshith <harshithl1777@gmail.com>
2023-12-02 14:16:57 -0500

    fix: minor changes in Instructor DAO, Session DAO, add new factories

--- src/main/java/relay/data_access/FirebaseInstructorDataAccessObject.java
+++ src/main/java/relay/data_access/FirebaseInstructorDataAccessObject.java
@@ -3,110 +3,147 @@ package relay.data_access;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import relay.exceptions.ResourceNotFoundException;
 import relay.entity.Instructor;
 
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
 /**
- * Provides data access methods for interacting with the Firebase Firestore database
+ * Provides data access methods for interacting with the Firebase Firestore
+ * database
  * to perform CRUD operations on Instructor entities.
  */
+@Validated
+@Component
 public class FirebaseInstructorDataAccessObject {
 
-    /**
-     * Saves the provided Instructor object to the Firestore database.
-     *
-     * @param instructor The Instructor object to be saved.
-     */
-    public void save(Instructor instructor) {
-        if (exists(instructor.getInstructorID())) {
-            update(instructor);
-        } else {
-            create(instructor);
-        }
-    }
+	Firestore db;
 
-    private void create(Instructor instructor) {
-        ApiFuture<DocumentReference> docRef = FirestoreSingleton.get().collection("instructors").add(instructor);
-        try {
-            instructor.setInstructorID(docRef.get().getId());
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
+	public FirebaseInstructorDataAccessObject() {
+		this.db = FirestoreSingleton.get();
+	}
 
-    private void update(Instructor instructor) {
-        FirestoreSingleton.get().collection("instructors").document(instructor.getInstructorID()).set(instructor);
-    }
+	/**
+	 * Saves the provided Instructor object to the Firestore database.
+	 *
+	 * @param instructor The Instructor object to be saved.
+	 */
+	public void save(Instructor instructor) {
+		if (exists(instructor.getInstructorID()))
+			update(instructor);
+		else
+			create(instructor);
+	}
 
-    /**
-     * Reads an Instructor object from the Firestore database based on the provided instructorID.
-     *
-     * @param instructorID The unique identifier of the Instructor.
-     * @return The retrieved Instructor object, or null if not found or an error occurs.
-     */
-    public Instructor read(String instructorID) {
-        ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID).get();
+	private void create(Instructor instructor) {
+		ApiFuture<DocumentReference> docRef = db.collection("instructors").add(instructor);
+		try {
+			instructor.setInstructorID(docRef.get().getId());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
 
-        try {
-            DocumentSnapshot instructorDocument = future.get();
-            if (instructorDocument.exists()) {
-                Instructor instructor = instructorDocument.toObject(Instructor.class);
-                assert instructor != null;
-                instructor.setInstructorID(instructorID);
-                return instructor;
-            } else {
-                return null;
-            }
+	private void update(Instructor instructor) {
+		String instructorID = instructor.getInstructorID();
+		if (instructorID == null)
+			throw new NullPointerException();
+		db.collection("instructors").document().set(instructor);
+	}
 
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	/**
+	 * Reads an Instructor object from the Firestore database based on the provided
+	 * instructorID.
+	 *
+	 * @param instructorID The unique identifier of the Instructor.
+	 * @return The retrieved Instructor object, or null if not found or an error
+	 *         occurs.
+	 * @throws ResourceNotFoundException if the instructor does not exist in
+	 *                                   Firestore
+	 * @throws NullPointerException      if the converted instructor document
+	 *                                   variable is null
+	 */
+	public Instructor read(String instructorID) {
+		if (instructorID == null)
+			throw new NullPointerException();
+		if (!exists(instructorID))
+			throw new ResourceNotFoundException("No such document exists.");
+		try {
+			ApiFuture<DocumentSnapshot> future = db.collection("instructors").document(instructorID)
+					.get();
+			DocumentSnapshot instructorDocument = future.get();
+			Map<String, Object> instructorDocumentData = instructorDocument.getData();
 
+			if (instructorDocumentData == null)
+				throw new NullPointerException();
 
-    /**
-     * Checks if an Instructor object with the provided instructorID exists in the Firestore database.
-     *
-     * @param instructorID The unique identifier of the Instructor.
-     * @return True if the Instructor exists, false otherwise.
-     */
-    public boolean exists(String instructorID) {
-        if (instructorID == null || instructorID.isEmpty()) {
-            return false;
-        }
-        ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID).get();
+			String firstName = (String) instructorDocumentData.get("firstName");
+			String lastName = (String) instructorDocumentData.get("lastName");
+			String emailAddress = (String) instructorDocumentData.get("emailAddress");
+			Instructor instructor = new Instructor(firstName, lastName, emailAddress);
 
-        try {
-            DocumentSnapshot instructorDocument = future.get();
-            return instructorDocument.exists();
+			instructor.setInstructorID(instructorID);
+			return instructor;
 
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    /**
-     * Deletes an Instructor object from the Firestore database based on the provided instructorID.
-     *
-     * @param instructorID The unique identifier of the Instructor to be deleted.
-     * @throws ResourceNotFoundException if the instructor does not exist in Firestore
-     */
-    public void delete(String instructorID) throws ResourceNotFoundException {
-        if (!exists(instructorID)) {
-            throw new ResourceNotFoundException("No such document exists.");
-        }
+	/**
+	 * Checks if an Instructor object with the provided instructorID exists in the
+	 * Firestore database.
+	 *
+	 * @param instructorID The unique identifier of the Instructor.
+	 * @return True if the Instructor exists, false otherwise.
+	 */
+	public boolean exists(String instructorID) {
+		if (instructorID == null || instructorID.isEmpty())
+			return false;
 
-        ApiFuture<WriteResult> deleteResult = FirestoreSingleton.get().collection("instructors").document(instructorID).delete();
-        try {
-            deleteResult.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			ApiFuture<DocumentSnapshot> future = db.collection("instructors")
+					.document(instructorID)
+					.get();
+			DocumentSnapshot instructorDocument = future.get();
+			return instructorDocument.exists();
+
+		} catch (ExecutionException | InterruptedException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Deletes an Instructor object from the Firestore database based on the
+	 * provided instructorID.
+	 *
+	 * @param instructorID The unique identifier of the Instructor to be deleted.
+	 * @throws ResourceNotFoundException if the instructor does not exist in
+	 *                                   Firestore
+	 * @throws NullPointerException      if the instructorID parameter is null
+	 */
+	public void delete(String instructorID) throws ResourceNotFoundException {
+		if (!exists(instructorID))
+			throw new ResourceNotFoundException("No such document exists.");
+
+		if (instructorID == null)
+			throw new NullPointerException();
+
+		try {
+			ApiFuture<WriteResult> deleteResult = db.collection("instructors").document(instructorID)
+					.delete();
+			deleteResult.get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/test/java/entity/CourseTest.java
+++ src/test/java/entity/CourseTest.java
@@ -2,75 +2,72 @@ package entity;
 
 import org.junit.jupiter.api.Test;
 import relay.entity.AttendanceRecord;
+import relay.entity.AttendanceRecordFactory;
 import relay.entity.Course;
 import relay.entity.Instructor;
-import relay.entity.Student;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CourseTest {
-    @Test
-    public void testGetCourseID() {
-        Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
-        assert course.getCourseID().equals("CSC 110");
-    }
+	@Test
+	public void testGetCourseID() {
+		Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
+		assert course.getCourseID().equals("CSC 110");
+	}
 
-    @Test
-    public void testGetCourseName() {
-        Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
-        assert course.getCourseName().equals("Software Engineering");
-    }
+	@Test
+	public void testGetCourseName() {
+		Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
+		assert course.getCourseName().equals("Software Engineering");
+	}
 
-    @Test
-    public void testGetInstructor() {
-        Instructor instructor = new Instructor("John", "Doe", "john@gmail.com");
-        Course course = new Course("CSC 110", "Software Engineering", instructor);
-        assert course.getInstructor().equals(instructor);
-    }
+	@Test
+	public void testGetInstructor() {
+		Instructor instructor = new Instructor("John", "Doe", "john@gmail.com");
+		Course course = new Course("CSC 110", "Software Engineering", instructor);
+		assert course.getInstructor().equals(instructor);
+	}
 
-    @Test
-    public void testGetHistory() {
-        Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
-        assert course.getHistory().isEmpty();
-    }
+	@Test
+	public void testGetHistory() {
+		Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
+		assert course.getHistory().isEmpty();
+	}
 
-    @Test
-    public void testSetCourseID() {
-        Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
-        course.setCourseID("CSC 207");
-        assert course.getCourseID().equals("CSC 207");
-    }
+	@Test
+	public void testSetCourseID() {
+		Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
+		course.setCourseID("CSC 207");
+		assert course.getCourseID().equals("CSC 207");
+	}
 
-    @Test
-    public void testSetCourseName() {
-        Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
-        course.setCourseName("Clean Architecture");
-        assert course.getCourseName().equals("Clean Architecture");
-    }
+	@Test
+	public void testSetCourseName() {
+		Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
+		course.setCourseName("Clean Architecture");
+		assert course.getCourseName().equals("Clean Architecture");
+	}
 
-    @Test
-    public void testSetInstructor() {
-        Instructor instructor = new Instructor("John", "Doe", "john@gmail.com");
-        Course course = new Course("CSC 110", "Software Engineering", instructor);
-        Instructor newInstructor = new Instructor("Jane", "Doe", "jane@gmail.com");
-        course.setInstructor(newInstructor);
-        assert course.getInstructor().equals(newInstructor);
-    }
+	@Test
+	public void testSetInstructor() {
+		Instructor instructor = new Instructor("John", "Doe", "john@gmail.com");
+		Course course = new Course("CSC 110", "Software Engineering", instructor);
+		Instructor newInstructor = new Instructor("Jane", "Doe", "jane@gmail.com");
+		course.setInstructor(newInstructor);
+		assert course.getInstructor().equals(newInstructor);
+	}
 
-    @Test
-    public void testSetHistory() {
-        Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
-        Timestamp createdAt = new Timestamp(System.currentTimeMillis());
+	@Test
+	public void testSetHistory() {
+		Course course = new Course("CSC 110", "Software Engineering", new Instructor("John", "Doe", "john@gmail.com"));
 
-        Student student = new Student("John", "Doe", "S12345", "john.doe@example.com");
+		AttendanceRecord attendanceRecord = AttendanceRecordFactory.createAttendanceRecord("John", "Doe", "S12345",
+				"john.doe@example.com");
+		List<AttendanceRecord> attendanceRecords = new ArrayList<>();
+		attendanceRecords.add(attendanceRecord);
 
-        AttendanceRecord attendanceRecord = new AttendanceRecord(student, createdAt);
-        List<AttendanceRecord> attendanceRecords = new ArrayList<>();
-        attendanceRecords.add(attendanceRecord);
-
-        course.setHistory(attendanceRecords);
-        assert course.getHistory().equals(attendanceRecords);
-        }
-    }
+		course.setHistory(attendanceRecords);
+		assert course.getHistory().equals(attendanceRecords);
+	}
+}
