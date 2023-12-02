8449d4fd74c9e8b7047e1c7bfe4e1adfd633996e
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-02 16:41:21 -0500

    interactor to execute the DAO call and get the list of courses

--- src/main/java/relay/show_courses/ShowCourseInteractor.java
+++ src/main/java/relay/show_courses/ShowCourseInteractor.java
@@ -1,4 +1,43 @@
 package relay.show_courses;
 
-public class ShowCourseInteractor implements ShowCourseInputBoundary{
+import relay.entity.Course;
+import relay.use_case.ExportCSVOutputData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowCourseInteractor implements ShowCourseInputBoundary {
+    final FirebaseCourseDataObjectInterface firebaseCourseDataObjectInterface;
+    final ShowCourseOutputBoundary outputBoundary;
+    final FirebaseCourseDataObjectInterface showCourseDataAccessInterface;
+
+
+    public ShowCourseInteractor(FirebaseCourseDataObjectInterface firebaseCourseDataObjectInterface, ShowCourseOutputBoundary outputBoundary, FirebaseCourseDataObjectInterface showCourseDataAccessInterface) {
+        this.firebaseCourseDataObjectInterface = firebaseCourseDataObjectInterface;
+        this.outputBoundary = outputBoundary;
+        this.showCourseDataAccessInterface = showCourseDataAccessInterface;
+    }
+
+    @Override
+    public void execute(ShowCourseInputData showCourseInputData) {
+        try {
+
+            List<Course> courses = firebaseCourseDataObjectInterface.getCoursesByInstructor("baldski");
+            List<String> courseNames = new ArrayList<>();
+            for (Course course: courses) {
+                String courseName = course.getCourseName();
+                courseNames.add(courseName);
+            }
+
+            ShowCourseOutputData outputData = new ShowCourseOutputData(courseNames);
+            outputBoundary.prepareSuccessView(outputData);
+
+        }  catch (Exception e) {
+            String errorMessage = "Error exporting attendance records: " + e.getMessage();
+            outputBoundary.prepareFailView(errorMessage);
+
+
+        }
+
+    }
 }
