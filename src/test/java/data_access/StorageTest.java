490ab308471555c18e5995669fc96851546b8550
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-12-01 19:22:29 -0500

    added 2 test to delete and view file in firebase storage

--- src/test/java/data_access/StorageTest.java
+++ src/test/java/data_access/StorageTest.java
@@ -1,17 +1,23 @@
 package data_access;
 
+import com.google.api.gax.paging.Page;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import org.junit.jupiter.api.Test;
 import relay.data_access.StorageSingleton;
+import relay.entity.Session;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StorageTest {
     @BeforeAll
@@ -21,7 +27,7 @@ public class StorageTest {
             FileInputStream firebaseCredentials = new FileInputStream(FIREBASE_CREDENTIALS_FILE);
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(firebaseCredentials))
-                    .setStorageBucket("blitz-web1.appspot.com")
+                    .setStorageBucket("relay-dev0.appspot.com")
                     .build();
 
             FirebaseApp.initializeApp(options);
@@ -56,5 +62,52 @@ public class StorageTest {
         assertNotNull(bucket1);
         assertNotNull(bucket2);
         assertSame(bucket1, bucket2);
+    }
+    @Test
+    public void testCreateQrCode() throws IOException {
+        Session session = new Session(null, "classID", null, null, "alphaNumericCode");
+        session.generateQRCode();
+        assert Session.checkIfImageExists("QRCodeNew.png");
+        Session.deleteFile("QRCodeNew.png");
+    }
+    @Test
+    public void testDeleteQrCodeImage() throws IOException {
+        Bucket initialBucket = StorageSingleton.get();
+        List<Blob> initialBlobs = getBlobsFromBucket(initialBucket);
+        Session session = new Session(null, "classID", null, null, "alphaNumericCode");
+        session.generateQRCode();
+        Session.deleteFile("QRCodeNew.png");
+        Bucket updatedBucket = StorageSingleton.get();
+        List<Blob> updatedBlobs = getBlobsFromBucket(updatedBucket);
+
+        assertBlobsEqual(initialBlobs, updatedBlobs);
+    }
+
+
+    private List<Blob> getBlobsFromBucket(Bucket bucket) {
+        List<Blob> blobsHold = new ArrayList<>();
+        Page<Blob> blobs = bucket.list();
+
+        for (Blob blob : blobs.iterateAll()) {
+           blobsHold.add(blob);
+        }
+        return blobsHold;
+    }
+
+    private void assertBlobsEqual(List<Blob> initialBlobs, List<Blob> updatedBlobs) {
+        assertEquals(initialBlobs.size(), updatedBlobs.size());
+
+        for (Blob initialBlob : initialBlobs) {
+            assertTrue(blobExistsInList(initialBlob, updatedBlobs));
+        }
+    }
+
+    private boolean blobExistsInList(Blob blob, List<Blob> blobList) {
+        for (Blob b : blobList) {
+            if (b.getName().equals(blob.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
