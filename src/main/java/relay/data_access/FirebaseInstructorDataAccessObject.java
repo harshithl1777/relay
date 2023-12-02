f50284beece7eaa132eaa819347336006918163a
Harshith <harshithl1777@gmail.com>
2023-12-02 00:16:30 -0500

    feat: add session data access object & merge storage bucket methods into session dao

--- src/main/java/relay/data_access/FirebaseInstructorDataAccessObject.java
+++ src/main/java/relay/data_access/FirebaseInstructorDataAccessObject.java
@@ -4,98 +4,106 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.WriteResult;
+
 import relay.entity.Instructor;
 
 import java.util.concurrent.ExecutionException;
 
 /**
- * Provides data access methods for interacting with the Firebase Firestore database
+ * Provides data access methods for interacting with the Firebase Firestore
+ * database
  * to perform CRUD operations on Instructor entities.
  */
 public class FirebaseInstructorDataAccessObject {
 
-    /**
-     * Saves the provided Instructor object to the Firestore database.
-     *
-     * @param instructor The Instructor object to be saved.
-     * @return True if the save operation is successful, false otherwise.
-     */
-    public boolean save(Instructor instructor) {
-        ApiFuture<DocumentReference> docRef = FirestoreSingleton.get().collection("instructors").add(instructor);
-        try {
-            instructor.setInstructorID(docRef.get().getId());
-            return true;
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	/**
+	 * Saves the provided Instructor object to the Firestore database.
+	 *
+	 * @param instructor The Instructor object to be saved.
+	 * @return True if the save operation is successful, false otherwise.
+	 */
+	public boolean save(Instructor instructor) {
+		ApiFuture<DocumentReference> docRef = FirestoreSingleton.get().collection("instructors").add(instructor);
+		try {
+			instructor.setInstructorID(docRef.get().getId());
+			return true;
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-    /**
-     * Reads an Instructor object from the Firestore database based on the provided instructorID.
-     *
-     * @param instructorID The unique identifier of the Instructor.
-     * @return The retrieved Instructor object, or null if not found or an error occurs.
-     */
-    public Instructor read(String instructorID) {
-        ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID).get();
+	/**
+	 * Reads an Instructor object from the Firestore database based on the provided
+	 * instructorID.
+	 *
+	 * @param instructorID The unique identifier of the Instructor.
+	 * @return The retrieved Instructor object, or null if not found or an error
+	 *         occurs.
+	 */
+	public Instructor read(String instructorID) {
+		ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID)
+				.get();
 
-        try {
-            DocumentSnapshot instructorDocument = future.get();
-            if (instructorDocument.exists()) {
-                Instructor instructor = instructorDocument.toObject(Instructor.class);
-                assert instructor != null;
-                instructor.setInstructorID(instructorID);
-                return instructor;
-            } else {
-                return null;
-            }
+		try {
+			DocumentSnapshot instructorDocument = future.get();
+			if (instructorDocument.exists()) {
+				Instructor instructor = instructorDocument.toObject(Instructor.class);
+				assert instructor != null;
+				instructor.setInstructorID(instructorID);
+				return instructor;
+			} else {
+				return null;
+			}
 
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
+	/**
+	 * Checks if an Instructor object with the provided instructorID exists in the
+	 * Firestore database.
+	 *
+	 * @param instructorID The unique identifier of the Instructor.
+	 * @return True if the Instructor exists, false otherwise.
+	 */
+	public boolean exists(String instructorID) {
+		ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID)
+				.get();
 
-    /**
-     * Checks if an Instructor object with the provided instructorID exists in the Firestore database.
-     *
-     * @param instructorID The unique identifier of the Instructor.
-     * @return True if the Instructor exists, false otherwise.
-     */
-    public boolean exists(String instructorID) {
-        ApiFuture<DocumentSnapshot> future = FirestoreSingleton.get().collection("instructors").document(instructorID).get();
+		try {
+			DocumentSnapshot instructorDocument = future.get();
+			return instructorDocument.exists();
 
-        try {
-            DocumentSnapshot instructorDocument = future.get();
-            return instructorDocument.exists();
+		} catch (ExecutionException | InterruptedException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	/**
+	 * Deletes an Instructor object from the Firestore database based on the
+	 * provided instructorID.
+	 *
+	 * @param instructorID The unique identifier of the Instructor to be deleted.
+	 * @return True if the delete operation is successful, false otherwise.
+	 */
+	public boolean delete(String instructorID) {
+		if (!exists(instructorID)) {
+			return false;
+		}
 
-    /**
-     * Deletes an Instructor object from the Firestore database based on the provided instructorID.
-     *
-     * @param instructorID The unique identifier of the Instructor to be deleted.
-     * @return True if the delete operation is successful, false otherwise.
-     */
-    public boolean delete(String instructorID) {
-        if (!exists(instructorID)) {
-            return false;
-        }
-
-        ApiFuture<WriteResult> deleteResult = FirestoreSingleton.get().collection("instructors").document(instructorID).delete();
-        try {
-            deleteResult.get();
-            return true;
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+		ApiFuture<WriteResult> deleteResult = FirestoreSingleton.get().collection("instructors").document(instructorID)
+				.delete();
+		try {
+			deleteResult.get();
+			return true;
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
 }
