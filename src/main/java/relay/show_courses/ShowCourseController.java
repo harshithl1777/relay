d164305c31efed0a88dcfd6398091ef596e5bb62
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-02 16:41:02 -0500

    controller to send req to frontend

--- src/main/java/relay/show_courses/ShowCourseController.java
+++ src/main/java/relay/show_courses/ShowCourseController.java
@@ -1,4 +1,17 @@
 package relay.show_courses;
 
+import relay.use_case.ExportCSVInputBoundary;
+import relay.use_case.ExportCSVInputData;
+
 public class ShowCourseController {
+    final ShowCourseInputBoundary  showCourseInteractor;
+
+    public ShowCourseController(ShowCourseInputBoundary showCourseInteractor) {
+        this.showCourseInteractor = showCourseInteractor;
+    }
+
+    public void execute(ShowCourseInputData inputData) {
+        showCourseInteractor.execute(inputData);
+
+    }
 }
