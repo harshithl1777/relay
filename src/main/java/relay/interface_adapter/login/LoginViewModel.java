485f43986f8f4fc29b2ee8aab5bbc066de3d22ca
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-12-03 02:09:20 -0500

    Implemented Login View Model and corresponding State

---
+++ src/main/java/relay/interface_adapter/login/LoginViewModel.java
@@ -0,0 +1,16 @@
+package relay.interface_adapter.login;
+
+import relay.interface_adapter.State;
+import relay.interface_adapter.ViewModel;
+
+public class LoginViewModel implements ViewModel {
+    @Override
+    public State getState() {
+        return null;
+    }
+
+    @Override
+    public void setState(State state) {
+
+    }
+}
