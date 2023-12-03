f10b3baf43b7fb3249f339747b1c33a61489590d
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-03 02:47:17 -0500

    API call responce handler created

--- src/main/java/relay/show_courses/ShowCourseResponceHandler.java
+++ src/main/java/relay/show_courses/ShowCourseResponceHandler.java
@@ -1,2 +1,23 @@
-package relay.show_courses;public class ShowCourseResponceHandler {
+package relay.show_courses;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import relay.data_access.FirebaseCourseDataAccessObject;
+import java.util.Map;
+
+public class ShowCourseResponceHandler {
+
+    @GetMapping("/api/instructors/{id}/courses")
+            public ResponseEntity<Map<String, Object>> showCourses(@PathVariable String instructorID) {
+
+        ShowCourseViewModel showCourseViewModel = new ShowCourseViewModel();
+
+        FirebaseCourseDataObjectInterface courseDataAccessObject = new FirebaseCourseDataAccessObject();
+
+        ShowCourseController showCourseController = ShowCourseFactory.createShowCourseUseCase(showCourseViewModel,
+                courseDataAccessObject);
+        ShowCourseInputData requestInputData = new ShowCourseInputData(instructorID);
+        return showCourseController.execute(requestInputData);
+    }
 }
