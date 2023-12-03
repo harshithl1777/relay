54202e439e9d7a9adbb2952a5b5f2876ca75126e
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-02 21:51:44 -0500

    updated state object

--- src/main/java/relay/show_courses/ShowCourseState.java
+++ src/main/java/relay/show_courses/ShowCourseState.java
@@ -1,14 +1,27 @@
 package relay.show_courses;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ShowCourseState {
     private List<String> courseNames;
 
+    // Default constructor or constructor with no arguments
+    public ShowCourseState() {
+        // Initialize the list or perform any other necessary setup
+    }
+
+
+    // Constructor that accepts course names
+    public ShowCourseState(List<String> courseNames) {
+        this.courseNames = courseNames;
+    }
+
+
+    // Getter and setter methods
     public void setCourseNames(List<String> courseNames) {
         this.courseNames = courseNames;
     }
+
     public List<String> getCourseNames() {
         return courseNames;
     }
