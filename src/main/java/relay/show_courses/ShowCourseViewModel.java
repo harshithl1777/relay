2eae171566735c006543ccb1a06fca1b93fe9be8
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-02 20:52:01 -0500

    view model with property change listeners for the view

---
+++ src/main/java/relay/show_courses/ShowCourseViewModel.java
@@ -0,0 +1,31 @@
+package relay.show_courses;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class ShowCourseViewModel extends ViewModel {
+
+    private ShowCourseState state = new ShowCourseState();
+
+    public ShowCourseViewModel() {
+        super("clear");
+    }
+
+    public void setState (ShowCourseState state) {
+        this.state = state;
+
+    }
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public ShowCourseState getState() {
+        return state;
+    }
+}
