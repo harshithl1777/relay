f135c9110a66686d88862611a789d7696639c163
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-03 14:42:29 -0500

    fixed exports

--- src/main/java/relay/interface_adapter/show_courses/ShowCourseController.java
+++ src/main/java/relay/interface_adapter/show_courses/ShowCourseController.java
@@ -1,13 +1,13 @@
-package relay.show_courses;
+package relay.interface_adapter.show_courses;
 
 import org.springframework.http.ResponseEntity;
-import relay.use_case.ExportCSVInputBoundary;
-import relay.use_case.ExportCSVInputData;
+import relay.use_case.show_courses.ShowCourseInputBoundary;
+import relay.use_case.show_courses.ShowCourseInputData;
 
 import java.util.Map;
 
 public class ShowCourseController {
-    final ShowCourseInputBoundary  showCourseInteractor;
+    final ShowCourseInputBoundary showCourseInteractor;
 
     public ShowCourseController(ShowCourseInputBoundary showCourseInteractor) {
         this.showCourseInteractor = showCourseInteractor;
