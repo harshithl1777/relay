f4e4d96bf78fd1888723790b2cb9fac93e200696
Muhammad Ibrahim <ibrahim.baluch@gmail.com>
2023-12-03 17:11:05 -0500

    added attendance to start session state

--- src/main/java/relay/interface_adapter/start_session/StartSessionState.java
+++ src/main/java/relay/interface_adapter/start_session/StartSessionState.java
@@ -1,26 +1,29 @@
 package relay.interface_adapter.start_session;
 
 import org.springframework.http.HttpStatus;
-
+import relay.entity.AttendanceRecord;
 import relay.interface_adapter.State;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class StartSessionState implements State {
     private String instructorID;
     private String sessionID;
     private String courseID;
     private Timestamp createdAt;
+    private ArrayList<AttendanceRecord> attendance;
     private String errorMessage;
     private HttpStatus statusCode;
 
-    public StartSessionState(StartSessionState copyState, String sessionID, String courseID, Timestamp createdAt) {
+    public StartSessionState(StartSessionState copyState, String sessionID, String courseID, Timestamp createdAt, ArrayList<AttendanceRecord> attendance) {
         this.instructorID = copyState.instructorID;
         this.errorMessage = copyState.errorMessage;
         this.statusCode = copyState.statusCode;
         this.sessionID = copyState.sessionID;
         this.courseID = copyState.courseID;
         this.createdAt = copyState.createdAt;
+        this.attendance = copyState.attendance;
     }
 
     public StartSessionState() {
@@ -40,6 +43,10 @@ public class StartSessionState implements State {
 
     public Timestamp getCreatedAt() {
         return createdAt;
+    }
+
+    public ArrayList<AttendanceRecord> getAttendance() {
+        return attendance;
     }
 
     @Override
@@ -69,6 +76,11 @@ public class StartSessionState implements State {
     public void setCreatedAt(Timestamp createdAt) {
         this.createdAt = createdAt;
     }
+
+    public void setAttendance(ArrayList<AttendanceRecord> attendance) {
+        this.attendance = attendance;
+    }
+
     @Override
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
