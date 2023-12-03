e37dba3b8c1c72a25de1437a144170a9c3006d0a
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-03 16:22:58 -0500

    input defined boundary and data

---
+++ src/main/java/relay/use_case/end_session/EndSessionInputBoundary.java
@@ -0,0 +1,9 @@
+package relay.use_case.end_session;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface EndSessionInputBoundary {
+    ResponseEntity<Map<String, Object>> execute(EndSessionInputData endSessionInputData);
+}
