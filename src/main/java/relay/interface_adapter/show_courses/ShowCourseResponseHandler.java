ef7f3081d3aebc400cf81b7887be527305295b7f
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-03 14:44:09 -0500

    final touches

--- src/main/java/relay/interface_adapter/show_courses/ShowCourseResponseHandler.java
+++ src/main/java/relay/interface_adapter/show_courses/ShowCourseResponseHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import relay.data_access.ShowCourseDataAccessAccess;
+import relay.data_access.FirebaseCourseDataAccessObject;
 import relay.use_case.show_courses.ShowCourseDataAccessInterface;
 import relay.app.ShowCourseFactory;
 import relay.use_case.show_courses.ShowCourseInputData;
@@ -15,15 +15,14 @@ import java.util.Map;
 public class ShowCourseResponseHandler {
 
     @GetMapping("/api/instructors/{id}/courses")
-    public ResponseEntity<Map<String, Object>> showCourses(@PathVariable String instructorID) {
-
+    public ResponseEntity<Map<String, Object>> showCourses(@PathVariable String id) {
         ShowCourseViewModel showCourseViewModel = new ShowCourseViewModel();
 
-        ShowCourseDataAccessInterface courseDataAccessObject = new ShowCourseDataAccessAccess();
+        ShowCourseDataAccessInterface courseDataAccessObject = new FirebaseCourseDataAccessObject();
 
         ShowCourseController showCourseController = ShowCourseFactory.createShowCourseUseCase(showCourseViewModel,
                 courseDataAccessObject);
-        ShowCourseInputData requestInputData = new ShowCourseInputData(instructorID);
+        ShowCourseInputData requestInputData = new ShowCourseInputData(id);
         return showCourseController.execute(requestInputData);
     }
 }
