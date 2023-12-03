9d81db7163705a14fed24bc3177822ab5702780c
vivic04 <117671628+vivic04@users.noreply.github.com>
2023-12-03 02:46:53 -0500

    fixed output boundary type

--- src/main/java/relay/show_courses/ShowCourseOutputBoundary.java
+++ src/main/java/relay/show_courses/ShowCourseOutputBoundary.java
@@ -1,11 +1,14 @@
 package relay.show_courses;
 
+import org.springframework.http.ResponseEntity;
 import relay.use_case.ExportCSVOutputData;
 
-public interface ShowCourseOutputBoundary {
-    void prepareSuccessView(ShowCourseOutputData outputData);
+import java.util.Map;
 
-    void prepareFailView(String error);
+public interface ShowCourseOutputBoundary {
+    ResponseEntity<Map<String, Object>> prepareSuccessResponce(ShowCourseOutputData outputData);
+
+    ResponseEntity<Map<String, Object>> prepareFailResponce(ShowCourseOutputData failureShowCourseOutputData);
 
 
 
