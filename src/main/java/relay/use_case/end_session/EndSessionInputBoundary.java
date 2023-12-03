49e7eb1a2024e51b80986f85ae73660732ba305b
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-12-03 18:15:55 -0500

    created end session use case

--- src/main/java/relay/use_case/end_session/EndSessionInputBoundary.java
+++ src/main/java/relay/use_case/end_session/EndSessionInputBoundary.java
@@ -1,10 +1,11 @@
 package relay.use_case.end_session;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
 
 public interface EndSessionInputBoundary {
-    ResponseEntity<Map<String, Object>> execute(EndSessionInputData endSessionInputData);
+    ResponseEntity<HttpStatus> execute(EndSessionInputData endSessionInputData);
 }
 
