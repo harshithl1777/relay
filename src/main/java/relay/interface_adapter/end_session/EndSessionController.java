aabbb073c6238e10254b91a30394ca172b9f622b
Muhammad Ibrahim <ibrahim.baluch@gmail.com>
2023-12-03 18:08:14 -0500

    implemented end session controller

---
+++ src/main/java/relay/interface_adapter/end_session/EndSessionController.java
@@ -0,0 +1,19 @@
+package relay.interface_adapter.end_session;
+
+import org.springframework.http.ResponseEntity;
+import relay.use_case.end_session.EndSessionInputBoundary;
+import relay.use_case.end_session.EndSessionInputData;
+
+import java.util.Map;
+
+public class EndSessionController {
+    final EndSessionInputBoundary endSessionUseCaseInteractor;
+
+    public EndSessionController(EndSessionInputBoundary endSessionUseCaseInteractor){
+        this.endSessionUseCaseInteractor = endSessionUseCaseInteractor;
+    }
+
+    public ResponseEntity<Map<String, Object>> execute(EndSessionInputData endSessionInputData) {
+        return endSessionUseCaseInteractor.execute(endSessionInputData);
+    }
+}
