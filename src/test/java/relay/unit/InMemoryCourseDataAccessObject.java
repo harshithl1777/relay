bf7abe4c3ce0bdbf073d473578c1348cf52701bc
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-12-04 10:44:37 -0500

    Fixed save method in all in memory DAO

--- src/test/java/relay/unit/InMemoryCourseDataAccessObject.java
+++ src/test/java/relay/unit/InMemoryCourseDataAccessObject.java
@@ -1,16 +1,22 @@
 package relay.unit;
 
 import relay.entity.Course;
+import relay.entity.Instructor;
+import relay.exceptions.ResourceAlreadyExistsException;
 import relay.exceptions.ResourceNotFoundException;
 import relay.use_case.create_course.CreateCourseCourseDataAccessInterface;
 import relay.use_case.end_session.EndSessionCourseDataAccessInterface;
+import relay.use_case.show_courses.ShowCourseCourseDataAccessInterface;
 import relay.use_case.start_session.StartSessionCourseDataAccessInterface;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class InMemoryCourseDataAccessObject implements CreateCourseCourseDataAccessInterface, StartSessionCourseDataAccessInterface, EndSessionCourseDataAccessInterface {
+public class InMemoryCourseDataAccessObject implements CreateCourseCourseDataAccessInterface, StartSessionCourseDataAccessInterface, EndSessionCourseDataAccessInterface, ShowCourseCourseDataAccessInterface {
     private final Map<String, Course> courses = new HashMap<>();
+
+    private static int nextID;
 
     public Course getCourseByID(String courseID) throws ResourceNotFoundException {
         for (Course course : courses.values()) {
@@ -23,11 +29,47 @@ public class InMemoryCourseDataAccessObject implements CreateCourseCourseDataAcc
 
     @Override
     public void save(Course course) {
-        courses.put(course.getCourseID(), course);
+        if (course.getCourseID() != null) {
+            courses.put(course.getInstructorID(), course);
+        } else {
+            setNextID();
+            course.setCourseID(String.valueOf(nextID));
+            courses.put(String.valueOf(nextID), course);
+        }
     }
+
+    private void setNextID() {
+        while (courses.containsKey(String.valueOf(nextID))) {
+            nextID++;
+        }
+
+    }
+
+//    public void save(Instructor instructor) throws ResourceAlreadyExistsException {
+//        if (instructor.getInstructorID() != null) {
+//            instructors.put(instructor.getInstructorID(), instructor);
+//        } else {
+//            setNextID();
+//            instructor.setInstructorID(String.valueOf(nextID));
+//            instructors.put(String.valueOf(nextID), instructor);
+//        }
+//
+//    }
+
 
     @Override
     public boolean exists(String courseID) {
         return courses.containsKey(courseID);
+    }
+
+    @Override
+    public ArrayList<Course> getCoursesByInstructor(String instructorID) {
+        ArrayList<Course> coursesRetrieved = new ArrayList<>();
+        for (Course course : courses.values()) {
+            if (course.getInstructorID().equals(instructorID)) {
+                coursesRetrieved.add(course);
+            }
+        }
+        return coursesRetrieved;
     }
 }
